b54e4e7cfc00c746931e21e1f4410e264f85f65d
Harrison Roberts <harrison.roberts@edu.sait.ca>
2018-09-06 13:23:38 -0600

    Week 1 lab, hello world project first commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ * @author Harrison Roberts
+ */
+public class HelloWorld {
+
+    /**
+     * Runs hello world
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
